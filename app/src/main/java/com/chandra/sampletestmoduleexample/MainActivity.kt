ce64788ccd88f3e26bf8ff227fb759d95d883bce
chandra <chandrashekhardhakite@gmail.com>
2020-04-14 13:42:34 -0400

    Created the sample exxample for Android lib and sample app

---
+++ app/src/main/java/com/chandra/sampletestmoduleexample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.chandra.sampletestmoduleexample
+
+import android.media.tv.TvContract
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.chandra.sampletestmoduleexample.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    val className:String = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+
+
+
+}
---
+++ logutils/src/main/java/com/chandra/logutils/LogUtils.kt
@@ -0,0 +1,9 @@
+package com.chandra.logutils
+
+import android.util.Log
+
+class LogUtils {
+    fun d( logClass:String, logStr:String){
+       Log.d(logClass,logStr)
+    }
+}
