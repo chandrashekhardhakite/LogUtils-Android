34a9778a0f147a6ccdb849514d8b2ecd86ed8e00
chandra <chandrashekhardhakite@gmail.com>
2020-04-14 15:08:51 -0400

    added more code

--- app/src/main/java/com/chandra/sampletestmoduleexample/MainActivity.kt
+++ app/src/main/java/com/chandra/sampletestmoduleexample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.media.tv.TvContract
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.chandra.logutils.LogUtils
 import com.chandra.sampletestmoduleexample.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     val className:String = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.main_activity)
 
         if (savedInstanceState == null) {
--- logutils/src/main/java/com/chandra/logutils/LogUtils.kt
+++ logutils/src/main/java/com/chandra/logutils/LogUtils.kt
@@ -2,8 +2,8 @@ package com.chandra.logutils
 
 import android.util.Log
 
-class LogUtils {
-    fun d( logClass:String, logStr:String){
+open class LogUtils {
+   open fun d( logClass:String, logStr:String){
        Log.d(logClass,logStr)
     }
 }
